d331d2c8674bff3b78de15930f09c31a94369418
vajrasami <vajrsami@gmail.com>
2023-12-12 19:21:42 +0545

    authetication and middleware

--- controller/contactController.js
+++ controller/contactController.js
@@ -35,4 +35,89 @@ const createContact = asyncHandler(async(req,res)=>
     
 });
 
-module.exports = {createContact};
+//for updating contact
+const contact_update = asyncHandler(async(req,res) =>
+{
+    try{
+        //params fetch unique Id
+        const contactId = req.params.contactId;
+        const updateContact = await Contact.findByIdAndUpdate(
+            contactId,
+            req.body,
+            { new:true}
+        );
+
+        if(!updateContact)
+        {
+            return res.status(404).json({message: "Contact not found"});
+        }
+        res.json({ message: "Contact updated successfully"});
+    }
+    catch(error)
+    {
+        res.status(500).json({mesage: error.message});
+    }
+});
+
+//for deleting
+const contact_delete = asyncHandler(async(req,res)=>
+{
+    try{
+        const contactId = req.params.contactId;
+        const deleteContact = await Contact.findByIdAndDelete(
+            contactId,
+            req.body,
+        );
+        if(!deleteContact)
+        {
+            return res.status(404).json({message:"Contact not found"});
+        }
+        res.json({message: "Contact deleted successfully"});
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message});
+    }
+});
+
+//fetch data
+const contact_all = asyncHandler(async(req,res )=>
+{
+    try{
+        const contacts = await Contact.find ();
+        if(contacts.length == 0)
+        {
+            return res.json({message: "There are no contact left"})
+        }
+        res.json(contacts);
+
+    }   
+    catch(error)
+    {
+        res.status(500).json({message: error.message});
+    }
+});
+
+//fetch single data
+const contact_single = asyncHandler(async(req,res )=>
+{
+    try{
+        const contactId = req. params.contactId;
+        const singleContact = await Contact.findById (
+            contactId,
+            req.body,
+        );
+        if(!singleContact)
+        {
+            return res.status(404).json({message: "Contact not found"});
+        }
+        res.json(singleContact);
+    }
+    catch(error)
+    {
+        res.status(500).json({mesage: error.message});
+
+    }
+});
+
+module.exports = {createContact, contact_delete, contact_update, contact_all, contact_single};
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,10 +1,18 @@
 //importing module
 const express = require("express");
 const router = express.Router();
-const {createContact} = require("../controller/contactController");
+const contactController = require("../controller/contactController");
 const validateToken = require ("../middleware/validateTokenHandler");
 
 router.use(validateToken);
-router.route("/").post(createContact);
+// router.route("/").post(createContact);
+
+router.post("/", contactController.createContact);
+router.get("/", contactController.contact_all);
+router.put("/:contactId",contactController.contact_update );
+router.delete("/:contactId",contactController.contact_delete);
+router.get("/:contactId",contactController.contact_single);
+
+
 
 module.exports = router;
